5a0f782aa6d93381545067f7db02e1dbb763c4b1
Daniel Keicher <dnlkch86@gmail.com>
2023-03-09 14:44:16 +0100

    fix: change schema, short_url datatype

--- src/models/short_url.js
+++ src/models/short_url.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 let hortUrlSchema = mongoose.Schema({
   original_url: String,
-  short_url: String
+  short_url: Number
 }, {collection: "short_urls"});
 
 module.exports = mongoose.model("ShortUrl", shortUrlSchema);
